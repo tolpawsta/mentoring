a4be30c7a7bd5f3126719620ea4ce5124305d630
tolpawsta <tolpawsta@gmail.com>
2020-07-09 15:44:42 +0300

    create project Exception Handling for output first symbol of the input string

---
+++ m3_exception_handling/ExceptionHandlingFirstCharOfString/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExceptionHandlingFirstCharOfString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
