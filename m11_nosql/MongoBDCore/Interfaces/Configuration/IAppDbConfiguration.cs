ce2045da2d18d893624d8d268a5f958c79ca7791
pavel_talochka <pavel_talochka@epam.com>
2020-10-14 00:28:41 +0300

    Create project MongoDBCore, add Book model

---
+++ m11_nosql/MongoBDCore/Interfaces/Configuration/IAppDbConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MongoBDCore.Interfaces.Configuration
+{
+    public interface IAppDbConfiguration
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
---
+++ m11_nosql/MongoBDCore/Models/Book.cs
@@ -0,0 +1,31 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MongoBDCore.Models
+{
+    public class Book
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        [Required(ErrorMessage = "Name is required")]
+        [BsonRequired]
+        public string Name { get; set; }
+        [BsonIgnoreIfNull(true)]
+        public string Author { get; set; }
+        [BsonIgnoreIfDefault(true)]
+        [BsonDefaultValue(0)]
+        public int Count { get; set; }
+        [BsonIgnoreIfNull]
+        public ICollection<string> Genres { get; set; }
+        [BsonIgnoreIfDefault(true)]
+        [BsonDefaultValue(0)]
+        public int Year { get; set; }
+        public Book()
+        {
+            Genres = new HashSet<string>();
+        }
+    }
+}
