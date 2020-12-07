c5276dbcce34943961b608b5f9f960842111a155
palw <tolpawsta@gmail.com>
2020-12-07 17:48:02 +0300

    Create HTTPWebGet.Librury and HTTPWebGet.UI

---
+++ m13_http_fundamentals/HTTPWebGet/HTTPWebGet.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HTTPWebGet.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
